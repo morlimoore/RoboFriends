d5acae965b9451a8c6daec2ed4137b2fa0664f77
morlimoore <cooliyk2@gmail.com>
2020-05-16 13:11:05 +0100

    Reverted to typicode API

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,23 +3,23 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary'
-import { robots } from '../robots';
+// import { robots } from '../robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots:robots,
+      robots:[],
       searchfield: ''
     }
   }
 
-  // componentDidMount() {
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //     .then(response=> response.json())
-  //     .then(users => {this.setState({ robots: users})});
-  // }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
